2f6a8d4f5f2291bc86a2ce4221c4780369004283
Lwmeek <timicivey27@gmail.com>
2022-10-19 10:48:40 -0400

    updated on click

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ buttons.forEach((button) => {
 		event.preventDefault();
 		const item = event.currentTarget.querySelector("#answer");
 		const arrow = event.currentTarget.querySelector(".arrows");
+		const header = event.currentTarget.querySelector("h2");
 		if (item.classList.contains("hide") === true) {
 			item.classList.remove("hide");
 			arrow.style.transform = "scale(-1, -1)";
+			header.style.fontWeight = "700";
 		} else {
 			item.classList.add("hide");
 			arrow.style.transform = "scale(1, 1)";
+			header.style.fontWeight = "400";
 		}
 	});
 });
